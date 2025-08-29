13bec1f1134b330cd1062c6fe12f5f85d32b0232
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-08-29 14:31:35 -0500

    salve paso1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario (nombre, apellido, dni, correo, fecha_registro) VALUES
+                                                                        ('Juan', 'Pérez', '12345678', 'juan.perez@gmail.com', NOW()),
+                                                                        ('María', 'Gonzales', '23456789', 'maria.gonzales@gmail.com', NOW()),
+                                                                        ('Luis', 'Ramírez', '34567890', 'luis.ramirez@gmail.com', NOW());
---
+++ src/test/java/hub/com/layeapi/LayeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.com.layeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
